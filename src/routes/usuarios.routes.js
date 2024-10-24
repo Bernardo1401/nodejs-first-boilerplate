df9845995e1859cb32632cb343bfd9692a481ee4
Bernardo Marques <sardinhaber@gmail.com>
2024-10-24 11:39:21 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,10 @@ const usuariosRoutes = Router();
 
 let usuarios = [];
 
-export default routes;
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message: usuarios.length == 0 ? "Não há usuários cadastrados" : `Total de usuários cadastrados: ${usuarios.length}`, usuarios,
+    });
+});
+
+export default usuariosRoutes;
